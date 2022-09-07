e2b363607a6553d4099ef6dc5da6a96bb0088457
Yasser Baltah <yasser.baltah@learnet.se>
2022-09-07 21:38:27 +0200

    first commit

--- function.py
+++ function.py
@@ -1,1 +1,16 @@
 
+def yasser():
+
+    a= float(input("Enter the first number: "))
+    b= float(input("Enter the seconde number: "))
+    c=input("what kind of math are you using: ")
+    if c== "divition":
+        r=(a/b)
+        print(r)
+    if c== "multiplication":
+        b=(a*b)
+
+    else:
+        n=(a+b)
+
+yasser()
